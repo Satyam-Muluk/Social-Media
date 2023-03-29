b11da3f4507e7829bca48f2de6fd9cb9fdb1330b
Satyam-Muluk <73930297+Satyam-Muluk@users.noreply.github.com>
2023-03-29 12:10:48 +0530

    adda controller and router for home

---
+++ controller/home_controller.js
@@ -0,0 +1,5 @@
+module.exports.home = function(req,res){
+    return res.end('<h1> Express is up for Codeal</h1>')
+}
+
+// moduel.exports.actionName = function(req,res)
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+// entry points to all the routes
+
+const express = require('express');
+
+const router = express.Router();
+const homeController = require('../controller/home_controller');
+
+router.get('/',homeController.home);
+
+
+
+
+module.exports = router;
+
